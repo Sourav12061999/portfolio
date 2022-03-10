71a2cd6d7d6d9c361ebcc57ac6c9e2494d68bcf5
Sourav Das <sdunity12061999@gmail.com>
2022-03-10 13:44:14 +0530

    complete

--- src/Data/projectsData.js
+++ src/Data/projectsData.js
@@ -6,6 +6,7 @@ import { DiNodejs } from "react-icons/di";
 import { DiMongodb, DiRedis } from "react-icons/di";
 import { SiExpress, SiSocketdotio, SiNextdotjs } from "react-icons/si";
 import { AiTwotoneFire } from "react-icons/ai";
+import { SiMaterialui } from "react-icons/si";
 const data = [
   {
     heading: "Nykaa Fashion Clone",
@@ -26,6 +27,17 @@ const data = [
     live: "https://fullstack-ecommerce-4tbzsb3u6-sourav12061999.vercel.app/",
     video: "",
     techStack: [SiNextdotjs, FaCss3, FaJsSquare, AiTwotoneFire],
+  },
+  {
+    heading: "Full Stack Clone of Udemy",
+    about: `This is the Full Stack Clone of the Famous Online Course Platform Udemy. This is a Full Stack website
+    where I have used React and Material UI in Front End and Node,Express and Mongoose in Backend. The Fontend is deployed in
+    Vercel and Backend is deployed in Heroku.`,
+    Github: "https://github.com/Sourav12061999/udemy-clone-front-end.git",
+    live: "https://udemy-clone-front-end.vercel.app/",
+    video: "",
+    techStack: [FaReact, SiMaterialui, SiExpress, DiMongodb, DiNodejs],
+    image: "udemy-pic.png",
   },
 ];
 
